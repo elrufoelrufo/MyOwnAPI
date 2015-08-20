e6b1a44a6da4951f737b4d7443f5678ad6327363
JOSE CARLOS RUANO BLANCO <josecarlosruanoblanco@MacBook-Pro-de-JOSE.local>
2015-08-20 15:43:12 +0200

    Creating our Controllers
    Creating Controllers for every route to our API using only the
    necessary methods

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+        return 'Vehicles on index';
+    }
+
+    
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,4 +23,5 @@ Route::resource('makers', 'MakerController', ['except' => ['create' , 'edit' ]])
 
 Route::resource('vehicles', 'VehicleController' , ['only' => ['index']]);
 
-Route::resource('makers.vehicles' , 'MakerVehiclesController'), ['except' => ['create' , 'edit' ]]; 
+
+Route::resource('makers.vehicles' , 'MakerVehiclesController' , ['except' => ['create' , 'edit' ]]); 
