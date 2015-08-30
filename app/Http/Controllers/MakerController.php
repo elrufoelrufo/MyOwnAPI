752425ebef9348eab93fe52ecfb5ec77afdb39cd
JOSE CARLOS RUANO BLANCO <josecarlosruanoblanco@MacBook-Pro-de-JOSE.local>
2015-08-30 19:55:15 +0200

    Implementing destroy methods

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -8,6 +8,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Maker;
+use App\Vehicle;
 
 use App\Http\Requests\CreateMakerRequest;
 
@@ -100,6 +101,23 @@ class MakerController extends Controller
      */
     public function destroy($id)
     {
-        //
+       $maker = Maker::find($id);
+
+        if (!$maker){
+            return response()->json (['message' => 'This maker does not exist' , 'code' => 404], 404 ); 
+        }
+        
+        $vehicles = $maker->vehicles;
+
+        if(sizeof($vehicles)>0){
+
+            return response()->json (['message' => 'This maker has associated vehicles. Delete his vehicles first. ' , 'code' => 409], 409 );//409 restriction code
+        }
+
+
+        $maker->delete();
+
+        return response()->json (['message' => 'This maker has been deleted'] , 200 );
+
     }
 }
